b369d7be1c78bf3da37eee40079a51db5fd1aaeb
Akshat Jain <jainakshat9578@gmail.com>
2026-09-17 23:47:43 +0530

    useState project

--- session-2/src/App.jsx
+++ session-2/src/App.jsx
@@ -12,7 +12,7 @@
 // // value abhi bhi 1 se hi increment hogi react sabko grp krdeta except last vala or last vala hi chalta hai!  
 //      seta((prev)=> prev+1)  
 //      seta((prev)=> prev+1)  
-//      seta((prev)=> prev+1)  
+//      seta((prev)=> prev+1)  // prev as current or previous state mil rhi hai
 // // ab value 3 se increase hogi this concept is known as Batch Updating!    
 //   }
   
@@ -41,27 +41,36 @@ import Card from './components/Card'
 import { useState } from 'react'
 
 const App = () => {
-  const [count, setcount] = useState(0)
   const [postData, setpostData] = useState([
     {
+    id:1,
     name: "Akshat",
     desc: "xdcfvgbhjcnd",
     likeCount: 0,
   },
   {
+    id:2,
     name: "Akshita",
     desc: "xdcfvgbhjcnd",
-    likeCount: 10,
+    likeCount: 0,
   }
 ])
 
-const like = ()=>{
-  setcount(count+1)
+const like = (id)=>{
+    setpostData((prev)=> prev.map((item)=>
+      item.id === id ? {...item,likeCount: item.likeCount+1} : item
+    ))
+}
+
+const dislike = (id)=>{
+  setpostData((prev)=> prev.map((item)=> 
+    item.id===id && item.likeCount>=1 ? {...item, likeCount: item.likeCount-1} : item
+))
 }
   
   return (
     <div className='px-10 py-5'>
-      <Card postData={postData} count={count} like={like}/>
+      <Card postData={postData} like={like} dislike={dislike}/>
     </div>
   )
 }
--- session-2/src/components/Card.jsx
+++ session-2/src/components/Card.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
 import { useState } from 'react'
 
-const Card = ({postData,like,count}) => {
+const Card = ({postData,like,dislike}) => {
     
   return (
     <div className='w-full flex gap-5'>
       {postData.map((item)=>{
         return (
-        <div className='card px-3 flex flex-col justify-center items-center w-60 h-60 bg-zinc-600 rounded-md'>
+        <div key={item.id} className='card px-3 flex flex-col justify-center items-center w-60 h-60 bg-zinc-600 rounded-md'>
         <img className='w-20 h-20 rounded-[50%]' src="https://images.unsplash.com/photo-1786381624438-da16f6cfb834?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw2fHx8ZW58MHx8fHx8" alt="" />
         <h1>{item.name}</h1>
         <p>{item.desc}</p>
-        <p>LikeCount : {count}</p>
+        <p>LikeCount : {item.likeCount}</p>
         <div className='w-full flex gap-5'>
-          <button onClick={like} className='px-8 py-3 bg-blue-500 rounded-md'>Like</button>
-        <button className='px-8 py-3 bg-red-500 rounded-md'>Dislike</button>
+          <button onClick={()=> like(item.id)} className='px-8 py-3 bg-blue-500 rounded-md'>Like</button>
+        <button onClick={()=> dislike(item.id)} className='px-8 py-3 bg-red-500 rounded-md'>Dislike</button>
         </div>
       </div>
         )
